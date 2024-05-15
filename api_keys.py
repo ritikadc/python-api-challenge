4efecba5ad74f702d99beb50b920a7de616b092c
Ritikadc <ritikajc@gmail.com>
2024-05-14 22:32:50 -0700

    Pushed the Changes.

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "49d84f47104e05d23d501f9bd4661f9b"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "1268b0e26dc142549cc9c4eec2ead8af"
